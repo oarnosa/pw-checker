92e1318feab970dec06febeb60f984ee457c2c5a
Orlando Arnosa <oarnosa@gmail.com>
2021-02-11 11:53:06 -0500

    Finds leak counts for hash values

--- checkmypass.py
+++ checkmypass.py
@@ -12,12 +12,18 @@ def request_api_data(query_char):
     return res
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        print(h, count)
+
+
 def pwned_api_check(password):
     # check password if it exists in api response
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     res = request_api_data(first5_char)
-    return res
+    return get_password_leaks_count(res, tail)
 
 
 # request_api_data('123')
